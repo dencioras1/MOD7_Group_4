988a5e8d4ca5e0124e7314fb3791c1ed0a8d9508
dencioras1 <denaskh@gmail.com>
2025-02-13 11:29:28 +0100

    Working game window.

---
+++ Breakout_Group_4/Main.py
@@ -0,0 +1,45 @@
+import pygame
+
+def main():
+
+    # Constant variables
+
+    FRAMERATE = 120
+    BACKGROUND = (0, 0, 0)
+    WINDOW_WIDTH = 1080
+    WINDOW_HEIGHT = 720
+    SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    # Code for game window settings
+
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    clock = pygame.time.Clock()
+
+    # Set name for game window
+
+    pygame.display.set_caption('BREAKOUT - G4')
+
+    # Main loop for the Breakout game
+
+    while(True):
+
+        # Check if the game window is closed
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # If the screen is closed, quit the program
+                pygame.quit()
+
+        # Fill screen with background color
+        screen.fill(BACKGROUND)
+
+        # Game logic goes here
+
+        # Updating game canvas/window
+        pygame.display.flip()
+
+        # Limiting framerate
+        clock.tick(FRAMERATE)
+
+if __name__ == "__main__":
+    main()
