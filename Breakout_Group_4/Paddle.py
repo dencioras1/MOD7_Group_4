1602ba7a335f99d005b487ef99d71981dda77d50
kveen2003 <k.l.veen@student.utwente.nl>
2025-02-14 12:03:52 +0100

    commitment

---
+++ Breakout_Group_4/Paddle.py
@@ -0,0 +1,11 @@
+
+class Paddle:
+    
+    def __init__(self, x, width, height, dx):
+        self.x = x
+        self.width = width
+        self.height = height
+        self.dx = dx  
+    
+    def __eq__(self, other):
+        pass
