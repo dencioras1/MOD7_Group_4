7fa52517a4ea6d41993d2958a62845eac9ff483f
dencioras1 <denaskh@gmail.com>
2025-02-24 17:14:17 +0100

    Adjusted x and y naming.

--- Breakout_Group_4/Paddle.py
+++ Breakout_Group_4/Paddle.py
@@ -2,36 +2,42 @@ import pygame
 
 class Paddle:
 
+    x = 0
+    y = 0
     width = 91
     height = 10
+    dx = 0
+    keys = None
+    colour = 0
+    col_rect = None
 
-    def __init__(self, x_loc, y_loc , dx, keys, colour):
-        self.x_loc = x_loc
-        self.y_loc = y_loc
+    def __init__(self, x, y , dx, keys, colour):
+        self.x = x
+        self.y = y
         self.width = Paddle.width
         self.height = Paddle.height
         self.dx = dx
         self.keys = keys
         self.colour = colour
-        self.col_rect = pygame.Rect(self.x_loc, self.y_loc, self.width, self.height)
+        self.col_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw_paddle(self, screen):
         pygame.draw.rect(screen, self.colour, self.col_rect)
 
     def update_paddle(self, pressed_keys):
-        if pressed_keys[self.keys[0]] and self.x_loc > 20: ##left movement
-            self.x_loc -= self.dx
-        if pressed_keys[self.keys[1]] and self.x_loc < 1060 - self.width: ##right movement
-            self.x_loc += self.dx
+        if pressed_keys[self.keys[0]] and self.x > 20: ##left movement
+            self.x -= self.dx
+        if pressed_keys[self.keys[1]] and self.x < 1060 - self.width: ##right movement
+            self.x += self.dx
 
-        self.col_rect = pygame.Rect(self.x_loc, self.y_loc, self.width, self.height)
+        self.col_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def set_location(self):
-        locations = [self.x_loc, self.y_loc]
+        locations = [self.x, self.y]
         return locations
 
     def get_x(self):
-        return self.x_loc
+        return self.x
     
     def get_width(self):
         return self.width
